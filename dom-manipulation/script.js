43c8a8695dae0210b62898244bec7101fac6d4bf
Josh <joshuakibwage23@gmail.com>
2024-10-18 09:37:53 +0300

    Add quote objects with text and category

---
+++ dom-manipulation/script.js
@@ -0,0 +1,8 @@
+
+//Array of quote objects where each has array has text and category
+const quotes = [
+    {text: 'It never gets easier you just get better, stronger and more resilient. Keep fighting the good fight.', category: "motivation"},
+    {text: 'The cold water does not get warmer if jump in late.', category: "motivation"},
+    {text: 'The path of self improvement is never ending but very rewarding.', category: "discipline"},
+    {text: 'The biggest risk is not taking a risk. In a world that\’s changing really quickly, the only strategy that is guaranteed to fail is not taking risks.', category: "life"}
+];
