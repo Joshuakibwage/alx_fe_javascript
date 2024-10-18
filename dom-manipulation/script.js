3ce96ca2bcdd36bce0ea2d03d911073a1311d219
Josh <joshuakibwage23@gmail.com>
2024-10-18 10:38:09 +0300

    Add function to display a random quote

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -6,3 +6,17 @@ const quotes = [
     {text: 'The path of self improvement is never ending but very rewarding.', category: "discipline"},
     {text: 'The biggest risk is not taking a risk. In a world that\’s changing really quickly, the only strategy that is guaranteed to fail is not taking risks.', category: "life"}
 ];
+
+//implementing functions to display a random quote
+
+function showRandomQuote(){
+     
+    const random = math.floor(math.random());
+      //get quote object
+    const randomQuote =  quotes[random];
+    //the div where the quote will be displayed
+    const quoteDisplay = document.getElementById('quoteDisplay');
+    //update the content of the div with text and category
+    quoteDisplay.innerHTML = `<p>${randomQuote.text}</p><p>${randomQuote.category}</p>`;
+}
+document.getElementById('newQuote').addEventListener('click', showRandomQuote);
